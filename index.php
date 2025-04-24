2d3b508e2d71d4ee948b26c0eec3c8e26914df13
Maggi <yuen.maggi@gmail.com>
2025-04-24 08:43:14 +0200

    second file with cookie exercise

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+//Aufgabe aus der DBE-Seite
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Cookie setzen und Namen aufrufen</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label for="name">Name: </label>
+        <input type="text" name="name" id="">
+        <input type="submit" value="">
+    </form>
+</body>
+</html>
