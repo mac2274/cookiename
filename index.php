01e2ba15f9df385cff13a5282769883db3f3c045
Maggi <yuen.maggi@gmail.com>
2025-04-24 09:18:13 +0200

    korrigiert mit hilfe von chatGPT

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<?php 
-//Aufgabe aus der DBE-Seite
-if (isset($_POST["submit"])) {
-    $name = $_POST['name'];
-
-    setcookie('username', $name, time() +3600);
-
-    header('index.php');
-    exit;
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,6 +6,25 @@ if (isset($_POST["submit"])) {
     <title>Cookie setzen und Namen aufrufen</title>
 </head>
 <body>
+<?php 
+//Aufgabe aus der DBE-Seite
+if (isset($_POST["submit"])) {
+    $name = $_POST['name'];
+
+    setcookie('username', $name, time() +3600);
+
+    header('location: index.php');
+    exit;
+}
+
+if (isset($_COOKIE['username'])){
+    $username = $_COOKIE['username'];
+
+    echo "<h1>Hallo, $username !</h1>";
+}
+?>
+
+
     <form action="index.php" method="POST">
         <label for="name">Name: </label>
         <input type="text" name="name" id="">
